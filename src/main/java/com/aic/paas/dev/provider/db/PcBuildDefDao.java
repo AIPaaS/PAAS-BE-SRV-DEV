b2d43126d0abf93e8eee83a5da277f713562a8ea
renfeng <renfeng@renfeng-PC>
2016-03-10 20:38:58 +0800

    研发管理代码生成

--- src/main/java/com/aic/paas/dev/provider/db/PcBuildDefDao.java
+++ src/main/java/com/aic/paas/dev/provider/db/PcBuildDefDao.java
@@ -1,15 +1,27 @@
 package com.aic.paas.dev.provider.db;
 
 
-import com.binary.framework.dao.Dao;
+import java.util.List;
+
 import com.aic.paas.dev.provider.bean.CPcBuildDef;
 import com.aic.paas.dev.provider.bean.PcBuildDef;
+import com.binary.framework.dao.Dao;
 
 
 /**
  * 构建定义表[PC_BUILD_DEF]数据访问对象
  */
 public interface PcBuildDefDao extends Dao<PcBuildDef, CPcBuildDef> {
+	
+	
+	/**
+	 * 跟据构建全名查询镜像
+	 * @param fullBuildName
+	 * @param cdt
+	 * @param orders
+	 * @return
+	 */
+	public List<PcBuildDef> selectListByFullBuildName(String fullBuildName, CPcBuildDef cdt, String orders);
 
 
 }
--- src/main/java/com/aic/paas/dev/provider/db/impl/PcBuildDefDaoImpl.java
+++ src/main/java/com/aic/paas/dev/provider/db/impl/PcBuildDefDaoImpl.java
@@ -1,17 +1,37 @@
 package com.aic.paas.dev.provider.db.impl;
 
 
-import com.binary.framework.dao.support.tpl.IBatisDaoTemplate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.aic.paas.dev.provider.bean.CPcBuildDef;
 import com.aic.paas.dev.provider.bean.PcBuildDef;
 import com.aic.paas.dev.provider.db.PcBuildDefDao;
+import com.binary.core.util.BinaryUtils;
+import com.binary.framework.dao.support.tpl.IBatisDaoTemplate;
 
 
 /**
  * 构建定义表[PC_BUILD_DEF]数据访问对象实现
  */
 public class PcBuildDefDaoImpl extends IBatisDaoTemplate<PcBuildDef, CPcBuildDef> implements PcBuildDefDao {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<PcBuildDef> selectListByFullBuildName(String fullBuildName,
+			CPcBuildDef cdt, String orders) {
+		BinaryUtils.checkEmpty(fullBuildName, "fullBuildName");
+		if(cdt == null) cdt = newCondition();
+		setDataStatusValue(cdt, 1);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("cdt", cdt);
+		fillCondition(cdt, map);
+		map.put("orders", orders);
+		map.put("fullBuildName", fullBuildName);
+		List<PcBuildDef> list = getSqlMapClientTemplate().queryForList(getTableName()+".selectListByFullBuildName", map);
+		return list;
+	}
 
 
 }
